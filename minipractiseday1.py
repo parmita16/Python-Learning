fa03a573df8d7f62f4e725acd601304fcff489a0
parmita16 <parmitarajthala2021@gmail.com>
2026-07-01 23:58:08 +0545

    Mini Practise Day 1

---
+++ minipractiseday1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+print(f"After 5 years, you will be {age + 5}.")
