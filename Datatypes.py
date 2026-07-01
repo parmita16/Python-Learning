a633d02c8527aad077c562e43a88924ca1859c1f
parmita16 <parmitarajthala2021@gmail.com>
2026-07-01 23:17:42 +0545

    Data Types

---
+++ Datatypes.py
@@ -0,0 +1,8 @@
+name = "John"      # str
+age = 25           # int
+price = 99.99      # float
+is_active = True   # bool
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(is_active))
