3ff0b26ddd17574184851126bd3694804aef565f
parmita16 <parmitarajthala2021@gmail.com>
2026-07-02 11:01:17 +0545

    Comparison Operators

---
+++ comparisonoperators.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+print("a =", a)
+print("b =", b)
+
+print("\nComparison Operators:")
+
+print("a == b :", a == b)   # Equal to
+print("a != b :", a != b)   # Not equal to
+print("a > b  :", a > b)    # Greater than
+print("a < b  :", a < b)    # Less than
+print("a >= b :", a >= b)   # Greater than or equal to
+print("a <= b :", a <= b)   # Less than or equal to
