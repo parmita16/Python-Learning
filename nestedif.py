2a03a4c4487fdab22a617ddede1854698a1aafb1
parmita16 <parmitarajthala2021@gmail.com>
2026-07-03 22:40:54 +0545

    nested if

---
+++ nestedif.py
@@ -0,0 +1,10 @@
+age = 20
+has_license = True
+
+if age >= 18:
+    if has_license:
+        print("You can drive.")
+    else:
+        print("Get a license first.")
+else:
+    print("Too young to drive.")
