e9ab8a23ef6cf3e6161d98abb9dc031c0850453e
parmita16 <parmitarajthala2021@gmail.com>
2026-07-31 21:54:27 +0545

    1d array in numpy

---
+++ 1darray.py
@@ -0,0 +1,6 @@
+#1D NumPy Array
+import numpy as np
+arr = np.arange(10, 51, 5)
+print("Array:", arr)
+print("3rd element:", arr[2])
+print("Last element:", arr[-1])
---
+++ gcdlcm.py
@@ -0,0 +1,10 @@
+import math
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+gcd = math.gcd(a, b)
+lcm = (a * b) // gcd
+
+print("GCD =", gcd)
+print("LCM =", lcm)
