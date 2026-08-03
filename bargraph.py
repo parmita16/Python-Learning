7a610f20829c8ee70608e7791322adc88b65b1da
parmita16 <parmitarajthala2021@gmail.com>
2026-08-03 20:57:23 +0545

    Bar Graph

---
+++ bargraph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = [1,2,3,4,5,6,7]
+y = [4,5,3,7,1,3,4]
+plt.figure(figsize = (9,6))
+plt.scatter(x,y,color = "red")
+plt.bar(x,y,color = "skyblue")
+plt.show()
